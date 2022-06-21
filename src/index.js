bb047a05628589d8b5d232ffb58e122c8144067f
Voyager101pw <voyager101sirius@gmail.com>
2022-06-21 23:19:07 +0500

    Creating a store (store part-4)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux';
+import rootReducer from './reducer';
 import './index.css'
 import App from './App'
 
 import './api/server'
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
