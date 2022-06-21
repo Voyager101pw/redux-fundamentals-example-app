aabb289ff4ee873fbfce8008edccd5f22102235f
Voyager101pw <voyager101sirius@gmail.com>
2022-06-21 06:43:49 +0500

    Spliting reducers (state-actions-reducers part-3)

---
+++ src/features/todos/todosSlice.js
@@ -0,0 +1,41 @@
+const initialState = {
+  todos: {
+    0: { text: 'Learn React', completed: true },
+    1: { text: 'Learn Redux', completed: false, color: 'purple' },
+    2: { text: 'Build something fun!', completed: false, color: 'blue' },
+  },
+  ids: [0, 1, 2]
+};
+
+
+const nextTodoId = (todos) => Object.keys(todos).length;
+
+export default function todoReucer(state = initialState, action) {
+  switch(action.type) {
+    case 'todos/todoAdded': {
+      return {
+          ...state,
+          todos: {
+            ...state.todos,
+            [nextTodoId(state.todos)]: {
+              text: action.payload,
+              completed: false,
+            },
+          }
+      }      
+    }
+     case 'todo/todoToggled': {
+      return {
+        ...state,
+        todos: {
+          ...state.todos,
+          [action.payload]: {
+            ...state.todos[action.payload],
+            completed: !state.todos[action.payload].completed,
+          }
+        }
+      }
+    }
+    default: return state;
+  }
+}
