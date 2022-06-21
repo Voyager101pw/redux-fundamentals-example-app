497205cf2ffca196ae23ddede2355f2ae943c90e
Voyager101pw <voyager101sirius@gmail.com>
2022-06-21 06:46:13 +0500

    Combining reducers (state-actions-reducers part-3)

--- src/reducer.js
+++ src/reducer.js
@@ -1,80 +1,9 @@
-// const initialState = {
-//   todos: [
-//     { id: 0, text: 'Learn React', completed: true },
-//     { id: 1, text: 'Learn Redux', completed: false, color: 'purple' },
-//     { id: 2, text: 'Build something fun!', completed: false, color: 'blue' },
-//   ],
-//   filters: {
-//     status: 'All',
-//     color: [],
-//   }
-// }
+import todoReducer from './features/todos/todosSlice';
+import filtersReducer from './features/filters/filtersSlice';
 
-const initialState = {
-  todos: {
-    0: { text: 'Learn React', completed: true },
-    1: { text: 'Learn Redux', completed: false, color: 'purple' },
-    2: { text: 'Build something fun!', completed: false, color: 'blue' },
-  },
-  ids: [0, 1, 2],
-  filters: {
-    status: 'All',
-    color: [],
-  }
-}
-
-const nextTodoId = (todos) => Object.keys(todos).length;
-
-export const rootReducer = (state = initialState, action) => {
-  switch(action.type) {
-    case 'todos/todoAdded': {
-      return {
-        ...state,
-        todos: [
-          ...state.todos,
-          {
-            id: nextTodoId(state.todos),
-            text: action.payload,
-            completed: false,
-          },
-        ]
-      }      
-    }
-    // case 'todo/todoToggled': {
-    //   return {
-    //     ...state,
-    //     todos: state.todos.map(todo => {
-    //       if (todo.id !== action.payload) {
-    //         return todo
-    //       }
-    //       return {
-    //         ...todo,
-    //         completed: !todo.completed,
-    //       }
-    //     })
-    //   }
-    // }
-    case 'todo/todoToggled': {
-      return {
-        ...state,
-        todos: {
-          ...state.todos,
-          [action.payload]: {
-            ...state.todos[action.payload],
-            completed: !state.todos[action.payload].completed,
-          }
-        }
-      }
-    }
-    case 'todo/todoFilterChanged': {
-      return {
-        ...state,
-        filters: {
-          ...state.filters,
-          status: action.payload
-        }
-      }
-    }
-    default: return state;
+export default function rootReducer(state = {}, action) {
+  return {
+    todos: todoReducer(state.todos, action),
+    filters: filtersReducer(state.filters, action),
   }
 }
